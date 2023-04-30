c433b54b103c4ee1d42ca890c8da8bbab3e1056b
Greg Solo <gregory.soloshchenko@gmail.com>
2023-04-30 17:36:26 -0400

    Make canvas resizable

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,11 @@ export async function main() {
   }
 
   const canvas = document.querySelector('canvas')
+  if (!canvas) {
+    fail('No canvas detected')
+    return
+  }
+
   const context = canvas?.getContext('webgpu') as GPUCanvasContext
   if (!context) {
     fail('No context detected')
@@ -24,8 +29,23 @@ export async function main() {
     format,
   })
 
-  simpleRender(context, device, getSimplePipeline(device, getSimpleVSFSModule(device), format))
   simpleComputeRender(device, getSimpleComputePipeline(device, getDoublingComputeModule(device)))
+
+  const pipeline = getSimplePipeline(device, getSimpleVSFSModule(device), format)
+  const observer = new ResizeObserver((entries) => {
+    for (const entry of entries) {
+      const canvas = entry.target as HTMLCanvasElement
+      const width = entry.contentBoxSize[0].inlineSize
+      const height = entry.contentBoxSize[0].blockSize
+
+      canvas.width = Math.min(width, device.limits.maxTextureDimension2D)
+      canvas.height = Math.min(height, device.limits.maxTextureDimension2D)
+
+      simpleRender(context, device, pipeline)
+    }
+  })
+
+  observer.observe(canvas)
 }
 
 main()
